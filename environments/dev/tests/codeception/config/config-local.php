823ae461bf7a56257b3211fb7c66ff90ea309325
Kostas Thermos <k.thermos@coolblue.nl>
2016-06-06 14:14:44 +0200

    Stop with the piping

--- environments/dev/tests/codeception/config/config-local.php
+++ environments/dev/tests/codeception/config/config-local.php
@@ -2,7 +2,6 @@
 return [
     'components' => [
         'db' => [
-            'dsn' => 'mysql:host=localhost;dbname=yii2_advanced_tests',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
